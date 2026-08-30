f86778146f59cea50fd3ea1c0d7e2825c022269f
Darius Jackony <dariusjackon11@gmail.com>
2026-08-30 22:34:46 +0300

    update

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,27 +1,28 @@
-// "use client"
-// import { useState } from "react"
-// import Button from "@/components/ui/button"
-// export default function Admin(){
-//     const [activeTab, setActiveTab] = useState("admissions")
-//     return(
-//         <section className="px-4 mt-20">
-//             <div>
-//                <Button
-//                 onClick={() => setActiveTab("admissions")}
-//                >
-//                  Admissions
-//                </Button>
-//                <Button
-//                 onClick={() => setActiveTab("admissions")}
-//                >
-//                  Admissions
-//                </Button>
-//                <Button
-//                 onClick={() => setActiveTab("admissions")}
-//                >
-//                  Admissions
-//                </Button>
-//             </div>
-//         </section>
-//     )
-// }
+"use client"
+import { useState } from "react"
+import Button from "@/components/ui/button"
+export default function Admin(){
+    const [activeTab, setActiveTab] = useState("admissions")
+    return(
+        <section className="px-4 mt-20">
+            <h2>admin</h2>
+            {/* <div>
+               <Button
+                onClick={() => setActiveTab("admissions")}
+               >
+                 Admissions
+               </Button>
+               <Button
+                onClick={() => setActiveTab("admissions")}
+               >
+                 Admissions
+               </Button>
+               <Button
+                onClick={() => setActiveTab("admissions")}
+               >
+                 Admissions
+               </Button>
+            </div> */}
+        </section>
+    )
+}
