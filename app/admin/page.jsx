8146f883f378506161c8738a1a65ed2c2de1cafd
Darius Jackony <dariusjackon11@gmail.com>
2026-08-30 22:39:45 +0300

    fix

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,29 +1,13 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import Button from "@/components/ui/button"
-export default function AdminPage(){
-    const [activeTab, setActiveTab] = useState("admissions")
-    return(
-        <section className="px-4 mt-20">
-            <h2>admin</h2>
-            {/* <div>
-               <Button
-                onClick={() => setActiveTab("admissions")}
-               >
-                 Admissions
-               </Button>
-               <Button
-                onClick={() => setActiveTab("admissions")}
-               >
-                 Admissions
-               </Button>
-               <Button
-                onClick={() => setActiveTab("admissions")}
-               >
-                 Admissions
-               </Button>
-            </div> */}
-        </section>
-    )
+import { useState } from "react";
+
+export default function AdminPage() {
+  const [activeTab, setActiveTab] = useState("admissions");
+
+  return (
+    <section className="px-4 mt-20">
+      <h2>Admin</h2>
+    </section>
+  );
 }
